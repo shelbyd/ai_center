308b51760ffcaa3a5e9316d55052be100e729db0
Shelby Doolittle <shelby@shelbyd.com>
2018-08-18 18:25:01 -0400

    force push to deploy

--- script/build_and_deploy.sh
+++ script/build_and_deploy.sh
@@ -7,6 +7,6 @@ elm-make Main.elm --output index.html
 git checkout -B gh-pages
 git add index.html
 git commit -m "deploy"
-git push origin gh-pages
+git push --force origin gh-pages
 
 git checkout master
