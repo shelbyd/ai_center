f3154c83b8171cc1904daa783f3002b09c10de8e
Shelby Doolittle <shelby@shelbyd.com>
2018-08-18 13:39:05 -0400

    add empty elm app

---
+++ Main.elm
@@ -0,0 +1,49 @@
+module Main exposing (..)
+
+import Html exposing (Html, button, div, text, pre, h1, input)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (value)
+
+
+main =
+    Html.beginnerProgram
+        { model =
+            { board = "- - -\n- - -\n- - -"
+            , play = Nothing
+            }
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    { board : String
+    , play : Maybe String
+    }
+
+
+type Msg
+    = UpdateBoard String
+    | ChangePlay String
+    | Play
+
+
+update msg model =
+    case msg of
+        UpdateBoard board ->
+            { model | board = board }
+
+        ChangePlay string ->
+            { model | play = Just string }
+
+        Play ->
+            { model | play = Nothing }
+
+
+view model =
+    div []
+        [ h1 [] [ text "AI Center" ]
+        , pre [] [ text model.board ]
+        , input [ value (Maybe.withDefault "" model.play), onInput ChangePlay ] []
+        , button [ onClick Play ] [ text "Play" ]
+        ]
